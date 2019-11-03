d92682ba4e2d29e157e6e2c7ba724ef8147558ac
gboriss <galinaborissenko@gmail.com>
2019-11-04 00:07:17 +0300

    ship

--- video.js
+++ video.js
@@ -1,25 +1,45 @@
 let video = document.getElementById('video')
+let viewport = document.getElementById('viewport')
 
-window.addEventListener('load', () => {
-	if (!video) return
 
-	video.width = window.innerWidth
-	video.height = window.innerHeight
+document.addEventListener('click', () => {
+	video.play()
 })
 
+let aspect = 16 / 9
+
+function setSize(W, H) {
+	if (!video) return
+
+	// console.log(W, H, condition, window.orientation)
+
+	document.body.style.width = W + 'px'
+	document.body.style.height = H + 'px'
+
+	if (W / H < aspect) {
+		video.height = H
+		video.width = H * aspect
+	
+		video.style.top = 0
+		video.style.left = (W - video.width) / 2 + 'px'
+	} else {
+		video.width = W
+		video.height = W * aspect
+	
+		video.style.left = 0
+		video.style.top = (H - video.height) / 2 + 'px'
+	}
+
+}
 
 
+setSize(window.innerWidth, window.innerHeight)
 
-// function onYouTubeIframeAPIReady() {
-//   let player;
-//   player = new YT.Player('player', {
-//     videoId: 'Xjs6fnpPWy4',
-//     playerVars: { 'autoplay': 1, 'controls': 0 },
-//     events: {
-//       'onReady': onPlayerReady,
-//       'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
-//       'onStateChange': onPlayerStateChange,
-//       'onError': onPlayerError
-//     }
-//   });
-// }
+window.addEventListener('resize', () => setSize(window.innerWidth, window.innerHeight))
+// window.addEventListener('orientationchange', () => {
+
+// 	setTimeout(() => {
+// 		if (window.orientation !== 0) setSize(window.innerHeight, window.innerWidth)
+// 		else setSize(window.innerWidth, window.innerHeight)
+// 	}, 1000)
+// })
