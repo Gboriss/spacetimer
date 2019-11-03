6a7a1fb05e26012f124d2115fd14fb337a0c0a12
gboriss <galinaborissenko@gmail.com>
2019-11-03 23:59:04 +0300

    pluralize

--- main.js
+++ main.js
@@ -1,58 +1,53 @@
-let days = document.getElementById('days');
-let dynamic = document.getElementById('dynamic');
-let day;
-let hours;
-let minutes;
-let seconds;
+let days = document.getElementById('days')
+let time = document.getElementById('time')
 
 let nextyear = new Date(
 	(new Date (Date.now()) ).getFullYear() + 1,
 	0, //month
 	1 // day
-);
+)
 
 // let lastone = nextyear.toString().split('').pop();
 
-setInterval(function () {getDays()}, 1000);
+setInterval(getDays, 1000)
+
 
 function getDays() {
 
-	let today = new Date(); 
-	let leftTime = nextyear.getTime() - today.getTime();
+	let today = new Date()
+
+	let leftTime = nextyear.getTime() - today.getTime()
 	
-	day = Math.floor( leftTime / (1000 * 60 * 60 * 24) ), // milliseconds per day 86400000
-	hours = pad(Math.floor( (leftTime / (1000 * 60 * 60)) % 24)),
-	minutes = pad(Math.floor( (leftTime / 1000 / 60) % 60 )),
-	seconds = pad(Math.floor( (leftTime / 1000) % 60 )),	
+	let day = Math.floor( leftTime / (1000 * 60 * 60 * 24) ) // milliseconds per day 86400000
+	let hours = pad(Math.floor( (leftTime / (1000 * 60 * 60)) % 24))
+	let minutes = pad(Math.floor( (leftTime / 1000 / 60) % 60 ))
+	let seconds = pad(Math.floor( (leftTime / 1000) % 60 ))
+
+	if (!days || !time) return
 	
-	days.innerHTML = day + " " + pluralize()
-	dynamic.innerHTML = hours + ":" + minutes + ":" + seconds; 
+	days.innerHTML = day + ' ' + pluralize(day, ['день', 'дня', 'дней'])
+	time.innerHTML = hours + ':' + minutes + ':' + seconds
 }
 
 function pad(n) {
-    return (n < 10 ? '0' : '') + n; //01,02
+    return (n < 10 ? '0' : '') + n // 01, 02
 }
 
-
-function pluralize (number, one, two, five) {
-	
-	number = day;
-	one = 'день';
-	two = 'дня';
-	five = 'дней';
+function pluralize(number, words) {
 
     if (number >= 5 && number <= 20) {
-        return five;
+        return words[2]
 	}
 
-    number %= 10; // no 21, 31
+	number %= 10 // no 21, 31
+
     if (number == 1) {
-        return one;
+        return words[0]
 	}
-	
-    if (number >= 2 && number <= 4) {
-        return two;
-    }
-	return five;
-} 
 
+    if (number >= 2 && number <= 4) {
+        return words[1]
+	}
+
+	return words[2]
+} 
